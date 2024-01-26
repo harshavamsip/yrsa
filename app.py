567b0280cc19f2227b6ea21ca62b89f0d811add5
harshavamsip <143535499+harshavamsip@users.noreply.github.com>
2024-01-26 10:28:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,6 @@ def analyze_and_categorize_comments(comments):
     return categorized_comments
 
 # Function to generate a word cloud from comments
-# Function to generate a word cloud from comments
 def generate_word_cloud(comments):
     if not comments:
         st.warning("No comments found for generating the word cloud.")
@@ -108,10 +107,7 @@ def generate_word_cloud(comments):
         st.warning("No valid text found for generating the word cloud.")
         return None
 
-    # Remove non-ASCII characters
-    all_comments = ''.join(char for char in all_comments if ord(char) < 128)
-
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(all_comments)
+    wordcloud = WordCloud(width=800, height=400, background_color='white', stopwords=set(stopwords.words('english'))).generate(all_comments)
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
@@ -121,6 +117,7 @@ def generate_word_cloud(comments):
     plt.savefig(tmp_file_path, bbox_inches='tight')
 
     return tmp_file_path
+
 
 
 # Streamlit web app
